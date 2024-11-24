030b6ae579f37bc24a58cd6a7e01b2e9c66585fe
ZihaoLiuNU <liu.zihao@northeastern.edu>
2024-11-24 17:52:07 -0500

    add server.js

--- nextai/server/chat.js
+++ nextai/server/chat.js
@@ -7,46 +7,48 @@ import { PromptTemplate } from "langchain/prompts";
 
 import { PDFLoader } from "langchain/document_loaders/fs/pdf";
 
-
 // NOTE: change this default filePath to any of your default file name
-const chat = async (filePath = "./uploads/hbs-lean-startup.pdf", query) => {
-// step 1:
-const loader = new PDFLoader(filePath);
+const chat = async (
+  query,
+  filePath = "./uploads/How_to_Speak_Bot_Prompt_Patterns_INFO_7375.pdf"
+) => {
+  // step 1:
+  const loader = new PDFLoader(filePath);
 
-const data = await loader.load();
+  const data = await loader.load();
 
-// step 2:
-const textSplitter = new RecursiveCharacterTextSplitter({
-chunkSize: 500, //  (in terms of number of characters) 
-chunkOverlap: 0,
-});
+  // step 2:
+  const textSplitter = new RecursiveCharacterTextSplitter({
+    chunkSize: 500, //  (in terms of number of characters)
+    chunkOverlap: 0,
+  });
 
-const splitDocs = await textSplitter.splitDocuments(data);
+  const splitDocs = await textSplitter.splitDocuments(data);
 
-// step 3
+  // step 3
 
-const embeddings = new OpenAIEmbeddings({
-openAIApiKey: process.env.REACT_APP_OPENAI_API_KEY,
-});
+  const embeddings = new OpenAIEmbeddings({
+    openAIApiKey: process.env.REACT_APP_OPENAI_API_KEY,
+  });
 
-const vectorStore = await MemoryVectorStore.fromDocuments(
-splitDocs,
-embeddings
-);
+  const vectorStore = await MemoryVectorStore.fromDocuments(
+    splitDocs,
+    embeddings
+  );
 
-// step 4: retrieval
+  // step 4: retrieval
 
-// const relevantDocs = await vectorStore.similaritySearch(
-// "What is task decomposition?"
-// );
+  // const relevantDocs = await vectorStore.similaritySearch(
+  // "What is task decomposition?"
+  // );
 
-// step 5: qa w/ customzie the prompt
-const model = new ChatOpenAI({
-modelName: "gpt-3.5-turbo",
-openAIApiKey: process.env.REACT_APP_OPENAI_API_KEY,
-});
+  // step 5: qa w/ customzie the prompt
+  const model = new ChatOpenAI({
+    modelName: "gpt-3.5-turbo",
+    openAIApiKey: process.env.REACT_APP_OPENAI_API_KEY,
+  });
 
-const template = `Use the following pieces of context to answer the question at the end.
+  const template = `Use the following pieces of context to answer the question at the end.
 If you don't know the answer, just say that you don't know, don't try to make up an answer.
 Use three sentences maximum and keep the answer as concise as possible.
 
@@ -54,16 +56,16 @@ Use three sentences maximum and keep the answer as concise as possible.
 Question: {question}
 Helpful Answer:`;
 
-const chain = RetrievalQAChain.fromLLM(model, vectorStore.asRetriever(), {
-prompt: PromptTemplate.fromTemplate(template),
-// returnSourceDocuments: true,
-});
+  const chain = RetrievalQAChain.fromLLM(model, vectorStore.asRetriever(), {
+    prompt: PromptTemplate.fromTemplate(template),
+    // returnSourceDocuments: true,
+  });
 
-const response = await chain.call({
-query,
-});
+  const response = await chain.call({
+    query,
+  });
 
-return response;
+  return response;
 };
 
 export default chat;
